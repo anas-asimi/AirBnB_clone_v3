fb69c05dd99ba53b69e1a2cb20962f5a42af4776
Anas Asimi <anas2020cr7@gmail.com>
2024-03-31 23:59:55 +0000

    Complete task 3. Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Flask index
+"""
+
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """ return status """
+    response = {"status": "OK"}
+    return jsonify(response)
