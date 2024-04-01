a237e0cbf64b0354c582d350d1f4887dac13f34c
Anas Asimi <anas2020cr7@gmail.com>
2024-04-01 01:36:03 +0000

    complete task 4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,29 @@ Flask index
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """ return status """
     response = {"status": "OK"}
+    return jsonify(response)
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """ return status """
+    response = {}
+    for clss in classes:
+        response[clss] = storage.count(classes[clss])
     return jsonify(response)
