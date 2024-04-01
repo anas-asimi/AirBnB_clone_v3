e55773f084b000a4f2f7c06c2849a9b961b678de
Anas Asimi <anas2020cr7@gmail.com>
2024-04-01 21:58:38 +0000

    fix functions descriptions and user_create function logic

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -3,6 +3,7 @@
 Flask users
 """
 
+from http.client import HTTPException
 from flask import jsonify, abort, request
 from werkzeug.exceptions import BadRequest
 from api.v1.views import app_views
@@ -22,7 +23,7 @@ def users():
 @app_views.route('/users/<user_id>', methods=['GET'],
                  strict_slashes=False)
 def user(user_id):
-    """ Retrieves a Amenity object """
+    """ Retrieves a User object """
     user = storage.get(User, user_id)
     if user:
         return jsonify(user.to_dict())
@@ -32,7 +33,7 @@ def user(user_id):
 @app_views.route('/users/<user_id>', methods=['DELETE'],
                  strict_slashes=False)
 def user_delete(user_id):
-    """ Deletes a Amenity object """
+    """ Deletes a User object """
     user = storage.get(User, user_id)
     if user:
         storage.delete(user)
@@ -44,16 +45,19 @@ def user_delete(user_id):
 @app_views.route('/users', methods=['POST'],
                  strict_slashes=False)
 def user_create():
-    """ Creates a Amenity """
+    """ Creates a User """
     try:
         user_dict = request.get_json()
-        user = User(name=user_dict['name'])
+        user = User(email=user_dict['email'], password=user_dict['password'])
         storage.new(user)
         storage.save()
         return jsonify(user.to_dict()), 201
     except Exception as ex:
         if isinstance(ex, KeyError):
-            abort(400, 'Missing name')
+            if 'email' in str(ex):
+                abort(400, 'Missing email')
+            if 'password' in str(ex):
+                abort(400, 'Missing password')
         if isinstance(ex, BadRequest):
             abort(400, 'Not a JSON')
         print('Exception :')
@@ -64,7 +68,7 @@ def user_create():
 @app_views.route('/users/<user_id>', methods=['PUT'],
                  strict_slashes=False)
 def user_update(user_id):
-    """ Updates a Amenity object """
+    """ Updates a User object """
     try:
         user = storage.get(User, user_id)
         if user is None:
