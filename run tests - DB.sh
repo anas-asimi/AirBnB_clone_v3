bb7683687204530633061f1d338334dd421f058c
Anas Asimi <anas2020cr7@gmail.com>
2024-04-02 15:22:35 +0000

    add scripts for running unittests

---
+++ run tests - DB.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# run unittests with database
+
+HBNB_ENV=test HBNB_MYSQL_USER=hbnb_test HBNB_MYSQL_PWD=hbnb_test_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_test_db HBNB_TYPE_STORAGE=db python3 -m unittest discover tests
---
+++ run tests - fileStorage.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# run unittests with filestorage
+
+python3 -m unittest discover tests
