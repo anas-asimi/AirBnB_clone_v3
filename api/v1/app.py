5d2a9fcd196dfe672a3c09735809d337e0b13351
Anas Asimi <anas2020cr7@gmail.com>
2024-04-02 01:18:55 +0000

    complete task 12. HTTP access control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,18 +7,35 @@ from os import getenv
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS, cross_origin
 
+# Global Flask Application (app)
 app = Flask(__name__)
-app.register_blueprint(app_views)
+
+# global jsonify prettier
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
+# global strict slashes
+app.url_map.strict_slashes = False
 
+# get environment variables
+HBNB_API_HOST = getenv("HBNB_API_HOST") or '0.0.0.0'
+HBNB_API_PORT = getenv("HBNB_API_PORT") or '5000'
+
+# Cross-Origin Resource Sharing
+cors = CORS(app, resources={r'/*': {'origins': HBNB_API_HOST}})
+
+# app_views register blueprint
+app.register_blueprint(app_views)
+
+# close db after requests
 @app.teardown_appcontext
 def close_storage(exception):
     """ after each request, this method calls storage.close() """
     storage.close()
 
 
+# 404 errors handlers
 @app.errorhandler(404)
 def page_not_found(error):
     """ return Not found """
@@ -27,8 +44,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    HBNB_API_HOST = getenv("HBNB_API_HOST")
-    HBNB_API_PORT = getenv("HBNB_API_PORT")
-    app.run(
-        host=HBNB_API_HOST or '0.0.0.0',
-        port=HBNB_API_PORT or '5000', threaded=True)
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
