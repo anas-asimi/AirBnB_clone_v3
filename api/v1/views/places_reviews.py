ab41e5e268b123dd319122844fcbe228210fb911
Anas Asimi <anas2020cr7@gmail.com>
2024-04-02 00:29:39 +0000

    remove unused import

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -7,7 +7,6 @@ from flask import jsonify, abort, request
 from werkzeug.exceptions import BadRequest
 from api.v1.views import app_views
 from models import storage
-from models.city import City
 from models.place import Place
 from models.user import User
 from models.review import Review
