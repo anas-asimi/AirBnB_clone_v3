c4a4b6f55d4e779f0c7818b63c65332e66e609f9
Anas Asimi <anas2020cr7@gmail.com>
2024-04-02 01:46:24 +0000

    fix pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,7 +45,8 @@ class FileStorage:
         """serializes __objects to the JSON file (path: __file_path)"""
         json_objects = {}
         for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict(used_by_fileStorage=True)
+            json_objects[key] = self.__objects[key].to_dict(
+                used_by_fileStorage=True)
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
